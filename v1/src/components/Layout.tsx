4abe3cacf65ad468b48c006a8d696fe0419c9fff
LordMirex <162094500+LordMirex@users.noreply.github.com>
2026-05-26 06:49:52 +0100

    feat(v1): add Layout component

---
+++ v1/src/components/Layout.tsx
@@ -0,0 +1,23 @@
+
+import { ReactNode } from 'react';
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: ReactNode;
+  showFooter?: boolean;
+}
+
+const Layout = ({ children, showFooter = true }: LayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navigation />
+      <main className="flex-1">
+        {children}
+      </main>
+      {showFooter && <Footer />}
+    </div>
+  );
+};
+
+export default Layout;
